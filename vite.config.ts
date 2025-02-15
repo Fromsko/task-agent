c98060d132541a6e22e8832d3ef65fa50740edf5
fromsko <1614355756@qq.com>
2025-02-15 19:51:11 +0800

    fix: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@layouts': '/src/layouts',
+    }
+  }
 });
